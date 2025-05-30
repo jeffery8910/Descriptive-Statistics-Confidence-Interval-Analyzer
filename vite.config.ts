a315f2ed7637abf7dd1953a4d0d284785958ae76
JEFFERY LIU <30743526+jeffery8910@users.noreply.github.com>
2025-05-31 00:06:03 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,14 @@
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 
+export default {
+  base: '/Descriptive-Statistics-Confidence-Interval-Analyzer/',
+}
+
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      base: '/Descriptive-Statistics-Confidence-Interval-Analyzer/',
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
